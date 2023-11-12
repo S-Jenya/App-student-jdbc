d523a8241d6e7d7a80c36f8a9827d9aa9ff11f30
Сёмин Евгений <s_j_n@bk.ru>
2023-11-12 15:48:52 +0500

    init

---
+++ src/main/java/com/sit/student/AppStudentJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.sit.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStudentJdbcApplication {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(AppStudentJdbcApplication.class, args);
+    }
+
+}
